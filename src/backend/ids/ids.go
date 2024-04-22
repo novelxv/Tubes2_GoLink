29ad0337b6ca74a79f061a1eb0a6fd8c2011a16b
shika <debrina.veisha@gmail.com>
2024-04-22 23:13:44 +0700

    add main route attribute on tree

--- src/backend/ids/ids.go
+++ src/backend/ids/ids.go
@@ -11,6 +11,7 @@ import (
 
 // main IDS function
 func Idsfunc(value string, goal string) *golink.GoLinkStats {
+	// save the start time
 	startTime := time.Now()
 	
 	level := 1 // note the current level
@@ -25,14 +26,18 @@ func Idsfunc(value string, goal string) *golink.GoLinkStats {
 	root.NewNodeLink(links)
 	stats := golink.NewGoLinkStats(root)
 
+	// add the root into the main route
+	root.AddMainRoute()
+
 	// do an iteration per level until the goal was found
 	for !found {
 		fmt.Printf("%d\n", level)
-		// root.SearhForGoal(&found, goal, level)
+		// searching for goal
 		found = SearhForGoal(root, &found, goal, level, stats)
-		level++
+		level++ // goal not found go to the next iteration
 	}
 
+	// goal found and set the runtime
 	stats.SetRuntime(time.Since(startTime))
 
 	if found {
@@ -69,12 +74,13 @@ func PrintTreeIds(n *tree.Tree) {
 
 // function to search the word goal recursively in IDS
 func SearhForGoal(n *tree.Tree, found *bool, goal string, level int, stats *golink.GoLinkStats) bool {
-	if level>=0 {
-		stats.AddTraversed()
+	// do recursive until reached level = 0
+	if level>=0 { 
+		stats.AddTraversed() // add node to traversed
 
 		if !n.Visited {
-			stats.AddChecked()
-			n.AddVisitedNode()
+			stats.AddChecked() // add node to checked
+			n.AddVisitedNode() // add node to visited
 			*found = tree.IsGoalFound(n.Value, goal) // check the current value with the goal
 		}
 
@@ -82,6 +88,7 @@ func SearhForGoal(n *tree.Tree, found *bool, goal string, level int, stats *goli
 	
 		// Goal founded
 		if *found {
+			n.AddMainRoute() // add the goal into the main route
 			fmt.Print("Found!!\n")
 			return true
 		}
@@ -95,12 +102,14 @@ func SearhForGoal(n *tree.Tree, found *bool, goal string, level int, stats *goli
 
 		// if goal not found yet, go to the next children
 		for _, child := range n.Children {
+			n.AddMainRoute() // add node to be the temporary main route
 			if(level>0){
 				SearhForGoal(child, found, goal, level-1, stats)
 			}
 			if *found {
 				break // break when the goal was found
 			}
+			n.UndoMainRoute() // goal was not founded undo from the main route
 		}
 
 	}
