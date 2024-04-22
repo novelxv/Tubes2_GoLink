016d1b4f6e46f3e5c368a58b1693862edc184221
angiekierra <angie.kierra@gmail.com>
2024-04-22 15:43:01 +0700

    feat:test fetching

--- src/backend/server.go
+++ src/backend/server.go
@@ -15,6 +15,11 @@ type InputData struct {
 	UseToggle bool   `json:"useToggle"`
 }
 
+type ResponseData struct {
+	StartLink string        `json:"startLink"`
+	EndLink   string        `json:"endLink"`
+	UseToggle bool          `json:"useToggle"`
+}
 
 // testing
 func main() {
@@ -31,25 +36,24 @@ func main() {
             c.JSON(400, gin.H{"error": "Invalid request payload"})
             return
         }
-
+		
         startLink := input.StartLink
 		endLink := input.EndLink
 		useToggle := input.UseToggle
-
-		// Send a response back to the frontend
-		c.JSON(200, gin.H{
-			"message": "Data received successfully",
-			"startLink": startLink,
-			"endLink": endLink,
-			"useToggle": useToggle,
-		})
-
+		
 		linkName := scraper.StringToWikiUrl(startLink) 
 		links, err := scraper.Scraper(linkName)
 		if err != nil {
 			log.Fatal("Error scraping:", err)
 		}
 		scraper.PrintLink(links) 
+		// Send a response back to the frontend
+		c.JSON(200, ResponseData {
+			StartLink: startLink,
+			EndLink:   endLink,
+			UseToggle: useToggle,
+		})
+
 
 	})
 	r.Run() 
